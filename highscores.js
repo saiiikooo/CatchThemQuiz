2eca3a7aec19958d2c30f991fe6557047bc45247
saiiikooo <cyrus.dato410@gmail.com>
2021-12-22 15:31:58 +1100

    updated files for end.html and end.js again

--- highscores.js
+++ highscores.js
@@ -1,1 +1,7 @@
-highscores.js
+const highScoreList = documnet.querySelector ('#highScoreList')
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+highScoreList.innerHTMl = 
+highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join('')
