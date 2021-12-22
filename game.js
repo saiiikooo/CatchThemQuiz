183606c491416792f4767c379d8c291edccdd58c
saiiikooo <cyrus.dato410@gmail.com>
2021-12-22 13:58:29 +1100

    added more functions and codes in game.js files

--- game.js
+++ game.js
@@ -69,3 +69,37 @@ let questions = [
         answer: 4,
     }
 ]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 7
+
+startGame = () => {
+    questionsCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === || questionsCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+        return window.location.assign('/end.html')
+
+    }
+    questionsCounter++
+    progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
+    progressBarFull.style.width = '${(questionCounter/MAX_QUESTIONS) * 100}%'
+
+    const questionIndex = Math.floor(Math.random() *availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText =currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionIndex, 1)
+
+    acceptingAnswers = true
+}
